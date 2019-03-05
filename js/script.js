91656b449504de4c5734c1872ae990948ed55340
csergey8 <c.sergey@protonmail.com>
2019-03-05 14:33:14 +0200

    convert jQuery code to an object literal module

--- js/script.js
+++ js/script.js
@@ -1,25 +1,72 @@
 ////////////////////////////////////////////
 ////////// Jquery bad code ////////////////
 /////////////////////////////////////////////
-var people = [];
-var template = $('#people-template').html();
+// (function() {
+//   var people = [];
+//   var template = $('#people-template').html();
 
-$('#peopleModule').find('button').on('click', function() {
-  people.push($('#peopleModule').find('input').val());
-  $('#peopleModule').find('input').val();
-  //data for mustache template
-  var data = {
-    people: people
-  };
-  $('#peopleModule').find('ul').html(Mustache.render(template, data));
-});
+// $('#peopleModule').find('button').on('click', function() {
+//   people.push($('#peopleModule').find('input').val());
+//   $('#peopleModule').find('input').val();
+//   //data for mustache template
+//   var data = {
+//     people: people
+//   };
+//   $('#peopleModule').find('ul').html(Mustache.render(template, data));
+// });
 
-$('#peopleModule').find('ul').delegate('i.del', 'click', function(e) {
-  var $remove = $(e.target).closest('li');
-  var i = $('#peopleModule').find('ul').find('li').index($remove);
+// $('#peopleModule').find('ul').delegate('i.del', 'click', function(e) {
+//   var $remove = $(e.target).closest('li');
+//   var i = $('#peopleModule').find('ul').find('li').index($remove);
 
-  $remove.remove();
-  people.splice(i, 1);
-});
+//   $remove.remove();
+//   people.splice(i, 1);
+// });
+  
+// })();
+
+
+(function() {
+  var people = {
+    people: ['Will', 'Loura'],
+    cacheDOM: function() {
+      this.$el = $('#peopleModule');
+      this.$button = this.$el.find('button');
+      this.$input = this.$el.find('input');
+      this.$ul = this.$el.find('ul');
+      this.template = this.$el.find('#people-template').html();
+    },
+    render: function() {
+      var data = {
+        people: this.people
+      };
+      this.$ul.html(Mustache.render(this.template, data));
+    },
+    bindEvents: function() {
+      this.$button.on('click', this.addPerson.bind(this));
+      this.$ul.delegate('i.del', 'click', this.deletePerson.bind(this))
+    },
+    addPerson: function() {
+      this.people.push(this.$input.val());
+      this.render();
+      this.$input.val('');
+    },
+    deletePerson: function(e) {
+      var $remove = $(e.target).closest('li');
+      var i = this.$ul.find('li').index($remove);
+
+      this.people.splice(i, 1);
+      this.render();
+    },
+    init: function() {
+      this.cacheDOM();
+      this.bindEvents();
+      this.render(); 
+    }
+  }
+  
+  people.init();
+})()
+
 
 
